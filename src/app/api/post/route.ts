494265a77b47a18d80ccbe8e6063c6be9c36274e
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-08-10 06:13:41 +0200

    deleted route post

--- src/app/api/post/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
-export async function GET() {
-  const user = await prisma.jobs.findMany();
-  return NextResponse.json(user);
-}
