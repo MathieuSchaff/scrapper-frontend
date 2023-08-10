10fd31441367841940e1f6afa07932f0ffc7a111
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-08-10 06:13:13 +0200

    testing protexted route with next auth

--- src/app/UserJobs/page.tsx
+++ src/app/UserJobs/page.tsx
@@ -1,7 +1,38 @@
+"use client"
 import React from 'react'
-
+import { useSession } from 'next-auth/react'
 export default function PageJobs() {
+
+  const { data: session } = useSession()
+  const fetchJobs = async () => {
+    const id = session?.user?.id
+    const accessToken = session?.user?.accessToken
+    // This will fetch the api/user/id endpoint from your Next.js server
+    // and return the data from the response
+    const res = await fetch(`http://localhost:3000/api/user/${id}`, {
+      headers: {
+        // Authorization: `Bearer ${accessToken}`
+        Authorization: `toto`
+      }
+    })
+    // const data = await res.json()
+    // console.log(data)
+  }
   return (
-    <div>PageJobs this is a protected route</div>
+    <div>PageJobs this is a protected route
+      <p>session: </p>
+      <p>protected route</p>
+      {JSON.stringify(session)}
+      <button
+        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+        onClick={() => {
+          console.log('fetch jobs')
+          fetchJobs()
+        }
+        }
+      >
+        fetch jobs
+      </button>
+    </div>
   )
 }
