7f760264c7fe2c277b94861642968b6f54b90d7e
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-08-10 22:21:00 +0200

    updating imports

--- src/components/Auth/SignInButton.tsx
+++ src/components/Auth/SignInButton.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react'
 import { signIn, signOut, useSession } from 'next-auth/react'
-import { Button } from './ui/button'
+import { Button } from '@/components/ui/button'
 export const SignInButton = () => {
   const { data: session } = useSession()
   console.log(session?.user.accessToken)
