a3497746dd74e716585a32f1b3de981ab91df95b
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-08-10 22:21:24 +0200

    better css

--- src/components/Jobs/Jobs.tsx
+++ src/components/Jobs/Jobs.tsx
@@ -2,7 +2,8 @@
 import { useState, useCallback } from 'react';
 import useSWR from 'swr'
 import { getJobs } from '@/lib/jobs/utils'
-import PaginationControls from './PaginationControls';
+import { MemoPaginationControls } from './PaginationControls';
+import Link from 'next/link';
 
 const Jobs = () => {
   const [pgnum, setPgnum] = useState(0);
@@ -26,19 +27,41 @@ const Jobs = () => {
     return <div>Loading...</div>;
   }
 
-
   return (
-    <div>
-      {
-        data && data.map((job, index) => (
-          <div key={index} className="flex flex-col items-center justify-center bg-white shadow-lg rounded-lg px-6 py-8">
-            <h2 className="text-2xl font-bold">{job.title}</h2>
-            <p className="text-gray-500">{job.link}</p>
-          </div>
-        ))
-      }
-      <PaginationControls pgnum={pgnum} setPageNumber={setPageNumber} />
-    </div>
+    <>
+
+      <MemoPaginationControls pgnum={pgnum} setPageNumber={setPageNumber} per_page={pgsize} />
+      <div className="flex flex-wrap justify-center">
+        {
+          data && data.map((job, index) => {
+            if (job.tags) {
+              try {
+                job.tags = JSON.parse(job.tags);
+              } catch (error) {
+                console.error("Error parsing JSON:", error);
+                job.tags = null;
+              }
+            }
+            return (
+              <div key={index} className="flex flex-col items-center justify-center bg-white shadow-lg rounded-lg px-6 py-8">
+                <h5 className="text-lg font-bold">{job.title}</h5>
+                <p className="text-gray-500">{job.location}</p>
+                <ul className="text-gray-500">
+                  {job.tags !== null && Array.isArray(job.tags) && job.tags.map((tag, index) => {
+                    return (
+                      <li key={index} className='bg-blue-600 text-white my-2'>{tag}</li>
+                    )
+                  })}
+                </ul>
+                <p className="text-gray-500">{job.company}</p>
+
+                <Link href={job?.link || ""} className="bg-blue-600 text-white rounded-md py-2 px-2">Web Link</Link>
+              </div>
+            )
+          })
+        }
+      </div>
+    </>
   )
 }
 export default Jobs
