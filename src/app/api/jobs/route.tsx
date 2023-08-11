4e68d18ac9826a609a4711db8869e003858bb9e2
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-08-11 10:23:11 +0200

    deleted console log in api/jobs route handler

--- src/app/api/jobs/route.tsx
+++ src/app/api/jobs/route.tsx
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server';
 import prisma from '../../../lib/prisma';
 export async function GET(request: Request) {
-  console.log('GET /api/jobs: ', request.url);
   const jobs = await prisma.job.findMany();
   return NextResponse.json(jobs);
 }
