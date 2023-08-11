faa54d2c90544923e3c715cd1715ddd3590834dd
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-08-11 10:22:11 +0200

    created a getJob function to get one job based on the id of the job

--- src/lib/jobs/utils.ts
+++ src/lib/jobs/utils.ts
@@ -1,7 +1,11 @@
 import { instanceAxios } from '@/lib/axiosConfig'
 import { Job } from '@prisma/client'
 import { AxiosResponse } from 'axios'
-
+export const getJob = async (args: string): Promise<Job> => {
+  const response: AxiosResponse<Job> = await instanceAxios.get<Job>(args);
+  const job: Job = response.data;
+  return job;
+}
 export const getTotalRowCount = async (args: string): Promise<number> => {
   const response: AxiosResponse<number> = await instanceAxios.get<number>(args);
   const count: number = response.data;
