223f276774ca1c999427b21fbb49b6ef5f36a9ac
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-08-11 10:21:06 +0200

    route count update

--- src/app/api/jobs/count/route.tsx
+++ src/app/api/jobs/count/route.tsx
@@ -1,6 +1,9 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 export async function GET(request: Request) {
+
   const count = await prisma.job.count();
+  console.log(count);
+  console.log(typeof count);
   return NextResponse.json(count);
 }
